9813e3953ccee45faebeaf241d4237130852fb31
Anastasiaza333 <anastasiaza05@gmail.com>
2022-12-07 15:48:56 +0200

    Project8.1

---
+++ UnitTest8.1/UnitTest8.1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Project8.1(ітер)/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest81
+{
+	TEST_CLASS(UnitTest81)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char str[8] = { "jwhilef" };
+			int t = Count(str);
+            Assert::AreEqual(t,1);
+		}
+	};
+}
